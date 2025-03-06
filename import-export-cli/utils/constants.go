f83c0acf40563aa58203eb6170441eff80f8903b
arshardh <arshardhifthikar50@gmail.com>
2025-03-07 00:37:55 +0530

    Add new default AI related endpoints

--- import-export-cli/utils/constants.go
+++ import-export-cli/utils/constants.go
@@ -193,7 +193,8 @@ const DefaultHttpRequestTimeout = 10000
 
 // AI
 const DefaultAIThreadCount = 3
-const DefaultAIEndpoint = "https://e95488c8-8511-4882-967f-ec3ae2a0f86f-prod.e1-us-east-azure.choreoapis.dev/lgpt/interceptor-service/interceptor-service-be2/v1.0"
+const DefaultAIEndpoint = "https://dev-tools.wso2.com/apim-ai-service/v2"
+const DefaultAITokenEndpoint = "https://api.asgardeo.io/t/wso2devtools/oauth2/token"
 
 // TLSRenegotiationNever : never negotiate
 const TLSRenegotiationNever = "never"
